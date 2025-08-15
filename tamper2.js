cf98cc157ed2fc9e58efc9b4266c833b75fb6f91
hermanka <hermanka.beta@gmail.com>
2025-08-15 11:21:51 +0700

    Update tamper2.js

--- tamper2.js
+++ tamper2.js
@@ -1,13 +1,21 @@
 console.log("Starting hook...")
 Java.perform(function(){
-    var Vault = Java.use("com.mobisystems.libfilemng.vault.Vault")
-    
-    for(var i=1000;i<9999;i++){
-        var ret_value = Vault.unlock(i.toString());
-        console.log("[+] testing PIN: "+i+" "+"ret_value: "+ ret_value);
-        if(ret_value!=null){
-            break;
+    var it = setInterval(function(){
+    try{
+        var Vault = Java.use("com.mobisystems.libfilemng.vault.Vault")        
+        for(var i=1000;i<9999;i++){
+            var ret_value = Vault.unlock(i.toString());
+            console.log("[+] testing PIN: "+i+" "+"ret_value: "+ ret_value);
+            if(ret_value!=null){
+                console.log("[*] PIN ditemukan: " + i);
+                clearInterval(it);
+                return;
+            }
         }
+     
+    } catch(e) {
+      console.log("failed!");
+      clearInterval(it);
     }
+  },200);
 })
-
